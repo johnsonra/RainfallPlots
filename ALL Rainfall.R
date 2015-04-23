a705b716fc6254ea5f5e35667f957f44726a3c0a
Johnson, Randall (NIH/NCI) [C] <johnsonra@mail.nih.gov>
2015-04-23 09:19:28 -0400

    Rounding out R package

--- ALL Rainfall.R
+++ ALL Rainfall.R
@@ -1,5 +1,6 @@
 #read ALL data into R
-data = read.table("/Volumes/AlexandrovEtAl/somatic_mutation_data/ALL/ALL_mutations_removed_by_filters.txt", na.strings = '', stringsAsFactors = FALSE, sep = '\t')
+data <- read.table("Data/ALL_clean_somatic_mutations_for_signature_analysis.txt", na.strings = '',
+                   stringsAsFactors = FALSE, sep = '\t')
 
 #convert chromosome into numeric
 data$V3 <- as.numeric(data$V3)
@@ -10,37 +11,9 @@ data <- data[order(data$V3, data$V4), ]
 #rename vectors
 names(data) <- c('V1', 'V2','chrom', 'pos', 'pos', 'V6', 'V7', 'V8')
 
-dat <- data
-
 #run Rainfall package
-Rainfall <- function(position, distance, cols = c('blue', 'black', 'orange', 'purple', 'yellow', 'green'), mutations = c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G')) 
-{
+library(RainfallPlot)
 
-
-dat <- dat[order(dat$pos),]
-
-position <- dat$pos
-
-#set the y axis equal to the difference in position data
-n <- length(dat$pos)
-distance <- c(NA, dat$pos[-1]-dat$pos[-n])
-
-#assign colors to the mutations
-cols <- c('blue', 'black', 'orange', 'purple', 'yellow', 'green')
-names(cols) <- c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G')
-
-#create rainfall plot
-plot(position,distance, xlab='genomic position', ylab='genomic distance', col=cols[dat$mutation], pch=16)
-
-#add a legend
-legend(1.25e+09, 2500000, title="Mutations", cex=0.75, pch=16, col=c("blue", "black", "orange", "purple", "yellow", "green"), legend= c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G'), ncol=6)
-
-#check that position & distance lengths are ok
-if(length(dat$pos) != length(dat$distance))
-	warning("length of position does not equal length of distance")
-	
-
-}
-
+rainfallPlot(data)
 
 
--- RainfallPlots/R/rainfall.R
+++ RainfallPlots/R/rainfall.R
@@ -1,17 +1,14 @@
-# rainfall.R
-# Create dummy data to start working on Rainfall Plots
-# Randall Johnson
-# BSP CCR Genetics Core at Frederick National Laboratory
-# CCR Collaborative Genetics Resource at NCI
-# Leidos Biomedical Research, Inc
-# Created January 28, 2015
-# Last Modified January 28, 2015
+# rainfallPlot.R
+# Create Rainfall Plot
+# Randall Johnson and Emily Mulhern
+# CCR Collaborative Genetics Resource
+# National Cancer Institute
 
-Rainfall <- function(position, distance, cols = c('blue', 'black', 'orange', 'purple', 'yellow', 'green'), mutations = c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G')) 
+
+rainfallPlot <- function(position, distance, cols = c('blue', 'black', 'orange', 'purple', 'yellow', 'green'),
+                         mutations = c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G'))
 {
-dat <- data.frame(pos = runif(n, 0, 3e9),
-                  mutation = sample(c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G'), size = n, replace = TRUE),
-                  stringsAsFactors = FALSE)
+
 
 dat <- dat[order(dat$pos),]
 
@@ -34,40 +31,6 @@ legend(1.25e+09, 2500000, title="Mutations", cex=0.75, pch=16, col=c("blue", "bl
 #check that position & distance lengths are ok
 if(length(dat$pos) != length(dat$distance))
 	warning("length of position does not equal length of distance")
-	
+
 
 }
-
-# number of sites
-n <- 10000
-
-# set random seed and generate data
-set.seed(928347)
-
-# this could be a lot better...should add chromosome numbers based on the genomic position
-dat <- data.frame(pos = runif(n, 0, 3e9),
-                  mutation = sample(c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G'), size = n, replace = TRUE),
-                  stringsAsFactors = FALSE)
-
-dat <- dat[order(dat$pos),]
-
-#set the x axis equal to position data
-x <- dat$pos
-
-#set the y axis equal to the difference in position data
-n <- length(dat$pos)
-y <- c(NA, dat$pos[-1]-dat$pos[-n])
-
-#assign colors to the mutations
-cols <- c('blue', 'black', 'orange', 'purple', 'yellow', 'green')
-names(cols) <- c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G')
-
-#create rainfall plot
-plot(x,y, xlab='genomic position', ylab='genomic distance', col=cols[dat$mutation], pch=16)
-
-#add a legend
-legend(1.25e+09, 2500000, title="Mutations", cex=0.75, pch=16, col=c("blue", "black", "orange", "purple", "yellow", "green"), legend= c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G'), ncol=6)
-
- 
- 
- 
