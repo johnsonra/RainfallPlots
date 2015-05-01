a43cf9bacf9bff3b63e4c2f96a5070e82be6b2de
emilymulhern <mulherner@fr-n-s069543.ncifcrf.gov>
2015-05-01 13:23:08 -0400

    updated RainfallPlot changes

--- ALL Rainfall.R
+++ ALL Rainfall.R
@@ -2,21 +2,11 @@
 data <- load('RainfallPlots/data/ALL.Rdata')
 data <- ALL
 
-#convert chromosome into numeric
-data$V3 <- as.numeric(data$V3)
-
-#order data
-data <- data[order(data$V3, data$V4), ]
-
-#rename vectors
-names(data) <- c('V1', 'V2','chrom', 'pos', 'pos', 'V6', 'V7', 'V8')
-
-data <- dat
 
 #run Rainfall package
 library(RainfallPlots)
 
-rainfallPlot(data)
+with(ALL, rainfallPlot(chrom, pos, paste(germline, somatic, sep = '>')))
 
 
 
--- RainfallPlots/R/rainfallPlot.R
+++ RainfallPlots/R/rainfallPlot.R
@@ -5,31 +5,38 @@
 # National Cancer Institute
 
 
-rainfallPlot <- function(position, distance, cols = c('blue', 'black', 'orange', 'purple', 'yellow', 'green'),
+rainfallPlot <- function(chrom, pos, mutation, cols = c('blue', 'black', 'orange', 'purple', 'yellow', 'green'),
                          mutations = c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G'))
 {
 
+ordered <- order(chrom, position)
 
-dat <- dat[order(dat$pos),]
+chrom <- chrom[ordered]
 
-position <- dat$pos
+pos <- pos[ordered]
+
+mutation <- mutation[ordered]
+
 
 #set the y axis equal to the difference in position data
-n <- length(dat$pos)
-distance <- c(NA, dat$pos[-1]-dat$pos[-n])
+n <- length(pos)
+distance <- c(NA, pos[-1]-pos[-n])
+
+distance <- ifelse(distance < 0, NA, distance)
+	
 
 #assign colors to the mutations
 cols <- c('blue', 'black', 'orange', 'purple', 'yellow', 'green')
 names(cols) <- c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G')
 
 #create rainfall plot
-plot(position,distance, xlab='genomic position', ylab='genomic distance', col=cols[dat$mutation], pch=16)
+plot(pos,distance, xlab='genomic position', ylab='genomic distance', col=cols[mutation], pch=16)
 
 #add a legend
 legend("topright", title="Mutations", cex=0.75, pch=16, col=c("blue", "black", "orange", "purple", "yellow", "green"), legend= c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G'), ncol=6)
 
 #check that position & distance lengths are ok
-if(length(dat$pos) != length(dat$distance))
+if(length(pos) != length(distance))
 	warning("length of position does not equal length of distance")
 
 
